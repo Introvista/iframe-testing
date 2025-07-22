f160fa6982105e2f4926f7a4450261dd91e97764
GalaGalaTCH <ivan.tsai21@gmail.com>
2025-07-22 16:53:17 +0800

    change pwa logo name

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,9 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['logo.png'],
       manifest: {
-        name: 'Vite PWA Project',
-        short_name: 'Vite PWA',
-        description: 'I am a simple Vite PWA',
+        name: 'Nespace',
+        short_name: 'Nespace',
+        description: 'Nespace',
         theme_color: '#ffffff',
         icons: [
           {
